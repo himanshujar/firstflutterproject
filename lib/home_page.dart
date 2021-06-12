24bb2cda6ad3c0dc88c89727038e709e7bd17704
Himanshu Raj <himanshujar870@gmail.com>
2021-06-13 00:15:56 +0530

    day 2 Completed

---
+++ lib/home_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  final int day = 20;
+  final String name = "Himanshu";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title:Text("Catalog App"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Hello  Himanshi $day by $name"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
