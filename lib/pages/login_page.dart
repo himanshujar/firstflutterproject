6d371eb32fc02d8b400f22c9dfbb564dfde8cc92
Himanshu Raj <himanshujar870@gmail.com>
2021-06-20 15:53:07 +0530

    day5complete

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -7,60 +7,64 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Material(
       color: Colors.white,
-      child: Column(
-        children: [
-          Image.asset(
-            "assets/images/login_image.png",
-            //scale:4
-            // height:200,
-            // width:200,
-            fit: BoxFit.cover,
-          ),
-          SizedBox(
-            height: 20,
-            child: Text("Hii"),
-          ),
-          Text(
-            "Welcome",
-            style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
+      child: SingleChildScrollView(
+              child: Column(
+          children: [
+            Image.asset(
+              "assets/images/login_image.png",
+              //scale:4
+              //height:500,
+              // width:200,
+              fit: BoxFit.cover,
             ),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
-            child: Column(
-              children: [
-                TextFormField(
-                  decoration: InputDecoration(
-                    hintText: "Enter Username",
-                    labelText: "Username",
-                  ),
-                ),
-                TextFormField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    hintText: "Enter Password",
-                    labelText: "Password",
-                  ),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                ElevatedButton(
-                  child: Text("Login"),
-                  style: TextButton.styleFrom(),
-                  onPressed: () {
-                    print("Hii Himanshu");
-                  },
-                )
-              ],
+            SizedBox(
+              height: 20,
+              child: Text("Hii"),
             ),
-          )
-        ],
+            Text(
+              "Welcome",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Enter Username",
+                      labelText: "Username",
+                    ),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: "Enter Password",
+                      labelText: "Password",
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  ElevatedButton(
+                    child: Text("Login"),
+                    
+                    style: TextButton.styleFrom(minimumSize: 
+                    Size(150, 50)),
+                    onPressed: () {
+                      print("Hii Himanshu");
+                    },
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
